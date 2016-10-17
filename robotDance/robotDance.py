13001e3c9a702aec6efdc83a8c9749127a0a16ae
Tej <tjoshi@chadwickschool.org>
2016-10-16 22:25:38 -0700

    Made the different commands work independently

--- robotDance/robotDance.py
+++ robotDance/robotDance.py
@@ -3,23 +3,28 @@ init ("sim")
 def danceMove1():
     forward(3,1)
     turnBy(180)
-danceMove1()
-    
-def danceMove2():
-    forward(3,1)
     forward(3,1)
     turnBy(180)
-danceMove2()
+    
+def danceMove2():
+    turnBy(180)
+    forward(3,1)
+    turnBy(180)
+    forward(3,1)
 
 def danceMove3():
-    forward(3,1)
     turnBy(90)
     forward(3,1)
     turnBy(180)
-danceMove3()
+    forward(3,1)
+    turnBy(270)
 
 def danceMove4():
-    forward(3,1)
+    turnBy(270)
     forward(3,1)
     turnBy(180)
-danceMove4()
+    forward(3,1)
+    turnBy(90)
+
+
+
