bb88d8be226d3892eeaef8f188401f0733b82b8d
Tej <tjoshi@chadwickschool.org>
2016-10-17 10:14:32 -0700

    Fixed a bunch of problems, added dance moves, and made the turns much faster

--- robotDance/robotDance.py
+++ robotDance/robotDance.py
@@ -28,30 +28,50 @@ def danceMove4():
 
 def danceMove5():
     x = 1
-    while x < 4:
-        turnBy(90)
+    while x < 5:
+        turnBy(2.45)
         forward(3,1)
-        turnBy(180)
-        turnBy(180)
+        turnBy(4.9,1)
+        turnBy(4.9,1)
         backward(3,1)
         
         x = x+1
         
 def danceMove6():
     x = 1
-    while x < 4:
-        turnBy(270)
+    while x < 5:
+        turnLeft(7.35,1)
         forward(3,1)
-        turnBy(180)
-        turnBy(180)
+        turnBy(4.9,1)
+        turnBy(4.9,1)
         backward(3,1)
         
         x = x+1
+
+        
+def danceMove7():
+    x = 1
+    while x < 5:
+        turnLeft(1.3,1)
+        forward(3,.5)
+        turnLeft(4.9,1)
+        turnLeft(4.9,1)
+        backward(3,1)
+        
+        x = x+1
+        
+def danceMove8():
+    x = 1
+    while x < 5:
+        turnRight(1.3,1)
+        forward(3,.5)
+        turnRight(4.9,1)
+        turnRight(4.9,1)
+        backward(3,1)
+        
+        x = x+1
+        
+
     
-danceMove6()
-
-
-  
-
-
-
+    
+    
