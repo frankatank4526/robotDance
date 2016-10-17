91b45d0f5f240440bf7304fbee8a5a779e8f67c9
Frank <fglantz2019@chadwickschool.org>
2016-10-16 22:47:32 -0700

    smae

--- robotDance/robotDance.py
+++ robotDance/robotDance.py
@@ -3,23 +3,37 @@ init ("sim")
 def danceMove1():
     forward(3,1)
     turnBy(180)
-danceMove1()
-    
-def danceMove2():
-    forward(3,1)
     forward(3,1)
     turnBy(180)
-danceMove2()
+    
+def danceMove2():
+    turnBy(180)
+    forward(3,1)
+    turnBy(180)
+    forward(3,1)
 
 def danceMove3():
-    forward(3,1)
     turnBy(90)
     forward(3,1)
     turnBy(180)
-danceMove3()
+    forward(3,1)
+    turnBy(270)
 
 def danceMove4():
-    forward(3,1)
+    turnBy(270)
     forward(3,1)
     turnBy(180)
-danceMove4()
+    forward(3,1)
+    turnBy(90)
+
+def danceMove5():
+    x = 1
+    while x < 4:
+        turnBy(90)
+        forward(3,1)
+        turnBy(180)
+        turnBy(180)
+        backward(3,1)
+        
+        x = x+1
+danceMove5()
