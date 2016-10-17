4c01a78fdfb840f22fbdfa3964e0eac5e6510c9e
Frank <fglantz2019@chadwickschool.org>
2016-10-16 23:54:24 -0700

    merged w/ tej branch

--- robotDance/robotDance.py
+++ robotDance/robotDance.py
@@ -1,17 +1,17 @@
 from Myro import *
 init ("sim")
-def danceMove1():
+def danceMove1():#movement right,comes back to origin
     forward(3,1)
     turnBy(180)
     forward(3,1)
     turnBy(180)
     
-def danceMove2():
+def danceMove2():#
     turnBy(180)
     forward(3,1)
     turnBy(180)
     forward(3,1)
-
+    
 def danceMove3():
     turnBy(90)
     forward(3,1)
@@ -28,12 +28,47 @@ def danceMove4():
 
 def danceMove5():
     x = 1
-    while x < 4:
-        turnBy(90)
+    while x < 5:
+        turnBy(2.45)
         forward(3,1)
-        turnBy(180)
-        turnBy(180)
+        turnBy(4.9,1)
+        turnBy(4.9,1)
         backward(3,1)
         
         x = x+1
-danceMove5()
+        
+def danceMove6():
+    x = 1
+    while x < 5:
+        turnLeft(7.35,1)
+        forward(3,1)
+        turnBy(4.9,1)
+        turnBy(4.9,1)
+        backward(3,1)
+        
+        x = x+1
+
+        
+def danceMove7():
+    x = 1
+    while x < 9:
+        turnLeft(1.3,1)
+        forward(3,.5)
+        turnLeft(4.9,1)
+        turnLeft(4.9,1)
+        backward(3,1)
+        
+        x = x+1
+        
+def danceMove8():
+    x = 1
+    while x < 9:
+        turnRight(1.3,1)
+        forward(3,.5)
+        turnRight(4.9,1)
+        turnRight(4.9,1)
+        backward(3,1)
+        
+        x = x+1
+        
+danceMove7()
